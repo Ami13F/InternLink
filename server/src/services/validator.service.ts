40619114b77ab9caf945c258f8acd245953cf9f6
Amelia Corina Forgacs <ameliaforgacs@gmail.com>
2020-11-30 11:06:30 +0200

    [Fix] Change min length

--- server/src/services/validator.service.ts
+++ server/src/services/validator.service.ts
@@ -1,6 +1,6 @@
-import {Credentials} from '../repositories';
-import isemail from 'isemail';
 import {HttpErrors} from '@loopback/rest';
+import isemail from 'isemail';
+import {Credentials} from '../repositories';
 
 export function validateCredentials(credentials: Credentials) {
   // Validate Email
@@ -9,9 +9,9 @@ export function validateCredentials(credentials: Credentials) {
   }
 
   // Validate Password Length
-  if (!credentials.password || credentials.password.length < 8) {
+  if (!credentials.password || credentials.password.length < 6) {
     throw new HttpErrors.UnprocessableEntity(
-      'password must be minimum 8 characters',
+      'password must be minimum 6 characters',
     );
   }
 }
