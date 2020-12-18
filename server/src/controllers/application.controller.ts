48fc0dc14bc2f41eb839a6a3044339ec5da7131b
Alex Dragan <35031007+aldragan0@users.noreply.github.com>
2020-12-18 21:16:15 +0200

    [feat] Add jobApplication controller

---
+++ server/src/controllers/application.controller.ts
@@ -0,0 +1,29 @@
+import {Filter, repository} from '@loopback/repository';
+import {get, getModelSchemaRef, param} from '@loopback/rest';
+import {JobApplication} from '../models';
+import {JobApplicationRepository} from '../repositories';
+
+export class JobApplicationController {
+  constructor(
+    @repository(JobApplicationRepository)
+    protected applicationRepository: JobApplicationRepository,
+  ) {}
+
+  @get('/applications', {
+    responses: {
+      '200': {
+        description: 'Get an Array of job Applications',
+        content: {
+          'application/json': {
+            schema: {type: 'array', items: getModelSchemaRef(JobApplication)},
+          },
+        },
+      },
+    },
+  })
+  async getApplications(
+    @param.query.object('filter') filter?: Filter<JobApplication>,
+  ): Promise<JobApplication[]> {
+    return this.applicationRepository.find(filter);
+  }
+}
